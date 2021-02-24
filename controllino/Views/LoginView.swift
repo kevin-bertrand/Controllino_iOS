ee03375117dbbe1b563e08dee37a9b75d746c0f1
Kevin Bertrand <k.bertrand@desyntic.com>
2021-02-24 09:51:04 +0100

    Project creation

---
+++ controllino/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  controllino
+//
+//  Created by Kevin Bertrand on 24/02/2021.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
