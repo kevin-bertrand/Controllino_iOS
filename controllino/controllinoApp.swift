06e52136883a3223f9f1e25b9af4177056b8ac0c
Kevin Bertrand <k.bertrand@desyntic.com>
2021-02-24 09:40:06 +0100

    Initial Commit

---
+++ controllino/controllinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  controllinoApp.swift
+//  controllino
+//
+//  Created by Kevin Bertrand on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct controllinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
